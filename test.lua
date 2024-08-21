3fda2e8b7865278354324b530ccd97f8787337af
Cleiton El Pato <148168642+PatoCleiton11@users.noreply.github.com>
2024-08-21 18:49:35 -0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,13 +1,13 @@
 for _, v in ipairs(workspace:GetDescendants()) do
   if v:IsA("BasePart") then
       local decal = Instance.new("Decal", v)
-      decal.Face = "Front"
+      decal.Face = Enum.NormalId.Front
       decal.Texture = "http://www.roblox.com/asset/?id=1365169976"
 
-      decal:Clone().Face = "Back"
-      decal:Clone().Face = "Left"
-      decal:Clone().Face = "Right"
-      decal:Clone().Face = "Top"
-      decal:Clone().Face = "Bottom"
+      decal:Clone().Face = Enum.NormalId.Back
+      decal:Clone().Face = Enum.NormalId.Left
+      decal:Clone().Face = Enum.NormalId.Right
+      decal:Clone().Face = Enum.NormalId.Top
+      decal:Clone().Face = Enum.NormalId.Bottom
   end
 end
