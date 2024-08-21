12bbfe17a949b3ed666a66c3fbbd45dcd2bea94f
Cleiton El Pato <148168642+PatoCleiton11@users.noreply.github.com>
2024-08-21 19:02:11 -0300

    Update test.lua
    fix

--- test.lua
+++ test.lua
@@ -10,22 +10,27 @@ for _, v in ipairs(workspace:GetDescendants()) do
             decal.Parent = v
         end
 
-        game.Lighting.MoonTextureId = "http://www.roblox.com/asset/?id=1365169976"
-        game.Lighting.SkyboxBk = "http://www.roblox.com/asset/?id=1365169976"
-        game.Lighting.SkyboxDn = "http://www.roblox.com/asset/?id=1365169976"
-        game.Lighting.SkyboxFt = "http://www.roblox.com/asset/?id=1365169976"
-       game.Lighting.SkyboxLf = "http://www.roblox.com/asset/?id=1365169976"
-       game.Lighting.SkyboxRt = "http://www.roblox.com/asset/?id=1365169976"
-       game.Lighting.SkyboxUp = "http://www.roblox.com/asset/?id=1365169976"
-       game.Lighting.SunTextureId = "http://www.roblox.com/asset/?id=1365169976"
+        local lighting = game:GetService("Lighting")
+
+        local sky = lighting:FindFirstChildOfClass("Sky")
+        if sky then
+            sky.MoonTextureId = "http://www.roblox.com/asset/?id=1365169976"
+            sky.SkyboxBk = "http://www.roblox.com/asset/?id=1365169976"
+            sky.SkyboxDn = "http://www.roblox.com/asset/?id=1365169976"
+            sky.SkyboxFt = "http://www.roblox.com/asset/?id=1365169976"
+            sky.SkyboxLf = "http://www.roblox.com/asset/?id=1365169976"
+            sky.SkyboxRt = "http://www.roblox.com/asset/?id=1365169976"
+            sky.SkyboxUp = "http://www.roblox.com/asset/?id=1365169976"
+            sky.SunTextureId = "http://www.roblox.com/asset/?id=1365169976"
+        end
     end
 end
 
 while wait() do
-  local message = Instance.new("Message", workspace)
-  message.Text = "IM"
-  wait(0.5)
-  message.Text = "BACK"
-  wait(1)
-  message:Destroy()
+    local message = Instance.new("Message", workspace)
+    message.Text = "IM"
+    wait(0.5)
+    message.Text = "BACK"
+    wait(1)
+    message:Destroy()
 end
