097b1da4c6074e3ad947743608792c075ca13c1c
Cleiton El Pato <148168642+PatoCleiton11@users.noreply.github.com>
2024-08-21 18:57:06 -0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,13 +1,31 @@
 for _, v in ipairs(workspace:GetDescendants()) do
-  if v:IsA("BasePart") then
-      local decal = Instance.new("Decal", v)
-      decal.Face = Enum.NormalId.Front
-      decal.Texture = "http://www.roblox.com/asset/?id=1365169976"
+    if v:IsA("BasePart") then
+        local faces = Enum.NormalId:GetEnumItems()
+        local decalTemplate = Instance.new("Decal")
+        decalTemplate.Texture = "http://www.roblox.com/asset/?id=1365169976"
 
-      decal:Clone().Face = Enum.NormalId.Back
-      decal:Clone().Face = Enum.NormalId.Left
-      decal:Clone().Face = Enum.NormalId.Right
-      decal:Clone().Face = Enum.NormalId.Top
-      decal:Clone().Face = Enum.NormalId.Bottom
-  end
+        for _, face in ipairs(faces) do
+            local decal = decalTemplate:Clone()
+            decal.Face = face
+            decal.Parent = v
+        end
+
+        game.Lightning.MoonTextureId = "http://www.roblox.com/asset/?id=1365169976"
+        game.Lightning.SkyboxBk = "http://www.roblox.com/asset/?id=1365169976"
+        game.Lightning.SkyboxDn = "http://www.roblox.com/asset/?id=1365169976"
+        game.Lightning.SkyboxFt = "http://www.roblox.com/asset/?id=1365169976"
+       game.Lightning.SkyboxLf = "http://www.roblox.com/asset/?id=1365169976"
+       game.Lightning.SkyboxRt = "http://www.roblox.com/asset/?id=1365169976"
+       game.Lightning.SkyboxUp = "http://www.roblox.com/asset/?id=1365169976"
+       game.Lightning.SunTextureId = "http://www.roblox.com/asset/?id=1365169976"
+    end
+end
+
+while wait() do
+  local message = Instance.new("Message", workspace)
+  message.Text = "IM"
+  wait(0.5)
+  message.Text = "BACK"
+  wait(1)
+  message:Destroy()
 end
